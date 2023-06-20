e4678ad5aa63d062458c1115d14f4ec1f941b9a6
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-06-20 15:13:18 -0400

    Se instala apollo server core y configura apollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,21 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      // debug: false,
-      // playground: false
+      playground: false,
+      // process.cwd es la carpeta donde se está ejecutando el proyecto
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // este plugin es para levantar el Apollo Studio
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+    ItemsModule,
   ],
   controllers: [],
   providers: [],
