46e2fa49d82fc3dd45c2a4b2596312f924e88881
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-06-24 14:21:32 -0400

    Se cambia tipo de dato para grapql

--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
 import { ItemsService } from './items.service';
 import { Item } from './entities/item.entity';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
@@ -20,7 +20,7 @@ export class ItemsResolver {
   }
 
   @Query(() => Item, { name: 'item' })
-  async findOne(@Args('id', { type: () => String }) id: string): Promise<Item> {
+  async findOne(@Args('id', { type: () => ID }) id: string): Promise<Item> {
     return this.itemsService.findOne(id);
   }
 
