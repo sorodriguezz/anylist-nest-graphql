15c8333317da454f3595fdff2a7ea6eac3b34171
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-06-24 13:33:58 -0400

    Se configura nest config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
