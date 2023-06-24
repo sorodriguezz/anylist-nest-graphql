dd112b737bf122ea585f6a6f0f2f73c779342300
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-06-24 14:19:55 -0400

    Se crea logica para buscar un item por id

--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -20,7 +20,7 @@ export class ItemsResolver {
   }
 
   @Query(() => Item, { name: 'item' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  async findOne(@Args('id', { type: () => String }) id: string): Promise<Item> {
     return this.itemsService.findOne(id);
   }
 
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
 import { Repository } from 'typeorm';
@@ -19,8 +19,10 @@ export class ItemsService {
     return this.itemsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({ id });
+    if (!item) throw new NotFoundException(`Item with id: ${id} not found`);
+    return item;
   }
 
   update(id: number, updateItemInput: UpdateItemInput) {
