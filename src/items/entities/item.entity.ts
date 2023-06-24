1678b062f39eb340eb962c419ee52163a9d4fb50
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-06-24 13:47:58 -0400

    Se crea entity items

--- src/items/entities/item.entity.ts
+++ src/items/entities/item.entity.ts
@@ -1,7 +1,22 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'items' })
 @ObjectType()
 export class Item {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field(() => String)
+  @Column()
+  name: string;
+
+  @Field(() => Int)
+  @Column()
+  quantity: number;
+
+  @Field(() => String)
+  @Column()
+  quantityUnits: string;
 }
