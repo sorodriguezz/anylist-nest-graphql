0a146194870ebbda3ceda96d1dfd7a3a6c3dcba0
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-06-27 19:52:00 -0400

    Se configura jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   providers: [AuthResolver, AuthService],
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule.register({ defaultStrategy: 'jwt' })],
 })
 export class AuthModule {}
