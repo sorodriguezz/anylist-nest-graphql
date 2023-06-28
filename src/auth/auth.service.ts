88475ea64d53657066123d7c424ff7fb45a20d71
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-06-27 20:14:28 -0400

    Se genera token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,25 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
-import { AuthResponse } from './types/auth-response.type';
-import { UsersService } from 'src/users/users.service';
-import { LoginInput, SignupInput } from './dto/inputs';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { LoginInput, SignupInput } from './dto/inputs';
+import { AuthResponse } from './types/auth-response.type';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  private getJwtToken(userId: string) {
+    return this.jwtService.sign({ id: userId });
+  }
 
   async signup(signupInput: SignupInput): Promise<AuthResponse> {
     const user = await this.usersService.create(signupInput);
 
-    const token = 'ABC123';
+    const token = this.getJwtToken(user.id);
 
     return {
       token,
@@ -28,7 +36,7 @@ export class AuthService {
       throw new BadRequestException('Email/Password do not match');
     }
 
-    const token = 'ABC123';
+    const token = this.getJwtToken(user.id);
 
     return {
       token,
